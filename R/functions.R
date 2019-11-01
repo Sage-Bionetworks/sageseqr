13b9095144e9cecd402ebeda1d65fca57ba9ff95
kelshmo <kelsey.montgomery@sagebase.org>
2019-11-01 18:15:46 +0000

    add config, functions to create boxplot

--- R/functions.R
+++ R/functions.R
@@ -1,29 +1,66 @@
 #'
 #'Detect file type and download data
 #'
-#'This function takes synIds and version number to download a series of rectangular files into a nested tibble. The file contents are
-#'stored nested in a variable filecontents.
+#'This function takes synIds and version number to download any rectangular file type from Synapse.
 #'
-#'@param inputs rectangular data where id = synId and version = file version number are required variables. Tidyverse argument required.
+#'@param synID A character vector with a Synapse Id.
+#'@param version Optional. A numeric vector with the Synapse file version number.
 #'@export
 #'@return A tibble.
 #'@examples
 #'\dontrun{
+#'file <- get_data(synID = "syn1234", version = 7)
 #'
-#'inputs <- tibble(object = c("count", metadata"), id = c("syn5678", "syn2345"), version = c("1", "8"))
-#'
-#'input_data <- get_data(inputs)
 #'}
-get_data <- function(inputs){
-  inputs %>%
-    mutate(thefile = purrr::map2(id, 
-                                 version, 
-                                 function(x,y) as_tibble(data.table::fread(synapser::synGet(x, version = y)$path))))
+get_data <- function(synID, version = NULL){
+  df <- as_tibble(data.table::fread(synapser::synGet(synID, version = as.numeric(version))$path))
+  df
 }
 #'
-#'Create covariate matrix from tidy metadata data frame. 
+#'Create covariate matrix from tidy metadata data frame.
 #'
-#'This function takes a tidy format.
-clean_covariates <- function(df, factors, continuous){
-  
+#'This function takes a tidy format. Coerces objects to correct type.
+#'
+#'@param md
+#'@param factors A vector of factor variables
+#'@param continuous A vector of continuous variables
+#' Remove primary variable for now - A vector of primary variables
+#'@param sample_variable A character vector with the column name corresponding to sample identifiers
+#'
+#'@export
+#'@return A data frame with coerced variables.
+#'@examples
+clean_covariates <- function(md, factors, continuous, sample_variable){
+  md[,factors] <- lapply(md[,factors], factor)
+  md[,continuous] <- lapply(md[,continuous], as.numeric)
+  md
 }
+#'
+#'Explore metadata by variable.
+#'
+#'This function produces boxplots from the variables provided.
+#'
+#'@param md
+#'@param vars A vector of variables to visualize
+#'
+#'@export
+#'@return
+#'@examples
+#' TO DO: test this
+boxplot_vars <- function(md, include_vars, x_var){
+  md %>%
+    dplyr::select(., c(include_vars, x_var)) %>%
+    gather(key, value, -x_var) %>%
+    ggplot(aes(x = x_var, y = value)) +
+    geom_boxplot() +
+    theme(legend.position = 'top') +
+    facet_grid(key ~ !! x_var, scales = "free")
+}
+#' Filter genes
+#'
+#'@param md A data frame with sample identifiers in a column
+#'@param count_matrix
+#'@param sample_variable
+#'
+#'
+
--- R/plan.R
+++ R/plan.R
@@ -1,7 +1,12 @@
+Sys.setenv(R_CONFIG_ACTIVE = "default")
+
 processing_plan <- drake_plan(
-  raw_data = targets(readr::read_csv(file_in("../../files_to_import.csv"))),
-  input_data = targets(get_data(raw_data)), 
-  clean = 
+  raw_data = targets("config.yml"),
+  clean_md = clean_covariates(metadata, factors = c("batch", "sex", "race", "spanish", "cogdx", "diagnosis","apoe4"),
+                              continuous = c("rincontinuous", "age_death", "pmi", "educ", "pct_pf_reads_aligned", "pct_coding_bases", "pct_intergenic_bases", "pct_intronic_bases", "pct_ribosomal_bases"),
+                              sample_variable = c("sampleid")),
+  covar_correlation = CovariateAnalysis::getAssociationStatistics(clean_md, PVAL = 0.05),
+
 )
 config <- drake_config(processing_plan)
 drake::vis_drake_graph(config)
