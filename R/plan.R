5ba745d4981c53c6a5f76d5e8e81024f6c528b89
kelshmo <kelsey.montgomery@sagebase.org>
2020-04-10 12:00:20 -0700

    feat amend plan to functions available

--- R/plan.R
+++ R/plan.R
@@ -8,9 +8,11 @@ plan <- drake_plan(
                               continuous = config::get("continuous")),
   #covar_correlation = CovariateAnalysis::getAssociationStatistics(clean_md, PVAL = 0.05),
   geneids = convert_geneids(count_df = counts),
-  biomart_results = get_biomart(geneids$ensembl_gene_id, host = "uswest.ensembl.org", organism = "hsa"),# Ensembl Release 99 (January 2020)
-  filtered_counts = filter_genes(md = clean_md, count_df = counts)
+  biomart_results = get_biomart(geneids$ensembl_gene_id, host = "ensembl.org", organism = "hsa"),# Ensembl Release 99 (January 2020)
+  filtered_counts = filter_genes(md = clean_md, count_df = counts),
+  cqn_counts = cqn(filtered_counts, biomart_results)
 )
 
-config <- drake::drake_config(plan)
-drake::vis_drake_graph(config, targets_only = TRUE)
+make_the_plan <- drake::drake_plan(plan)
+drake::vis_drake_graph(make_the_plan, targets_only = TRUE)
+
