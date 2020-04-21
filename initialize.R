fedc77fddbd8df5dc14229d1346ba53f0ff8be9d
kelshmo <kelsey.montgomery@sagebase.org>
2020-04-20 18:31:16 -0700

    rm deprecated drake::drake_config

--- initialize.R
+++ initialize.R
@@ -1,4 +1,4 @@
-Sys.setenv(R_CONFIG_ACTIVE = "default")
+Sys.setenv(R_CONFIG_ACTIVE = "mayo")
 
 # TO DO: how to setup cache for end user
 # make_cache
@@ -10,4 +10,8 @@ synLogin()
 
 source("R/plan.R")
 
+# Run the analysis
 drake::make(plan)
+
+# Visualize the results
+drake::vis_drake_graph(plan, targets_only = TRUE)
