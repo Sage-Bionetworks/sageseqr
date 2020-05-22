058afae3f4ca29d1581abc3085728f731d232467
kelshmo <kelsey.montgomery@sagebase.org>
2020-05-22 16:44:37 -0700

    change import of data with intermediate check_targets step

--- initialize.R
+++ initialize.R
@@ -1,15 +1,18 @@
-Sys.setenv(R_CONFIG_ACTIVE = "mayo")
+Sys.setenv(R_CONFIG_ACTIVE = "default")
 
 # TO DO: how to setup cache for end user
 # make_cache
 
-source("R/packages.R")
 source("R/functions.R")
 
 # Login to Synapse. Make a Synapse account and use synaper to login: https://r-docs.synapse.org/articles/manageSynapseCredentials.html
 synapser::synLogin()
 
 source("R/plan.R")
+
+# Check for changes to the config file
+config <- config::get()
+source("check_targets.R")
 
 # Run the analysis
 drake::make(plan)
