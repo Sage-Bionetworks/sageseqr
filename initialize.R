039e4a16a1c2d9dbd28839f628d221215da6c072
kelshmo <kelsey.montgomery@sagebase.org>
2020-06-04 18:50:13 -0700

    call drake plan as function

--- initialize.R
+++ initialize.R
@@ -9,13 +9,24 @@ source("R/plan.R")
 # Login to Synapse. Make a Synapse account and use synaper to login: https://r-docs.synapse.org/articles/manageSynapseCredentials.html
 synapser::synLogin()
 
-# Check for changes to the config file
-config <- config::get()
-source("check_targets.R")
-
 # Run the analysis
-drake::make(execute_plan(config = config))
+plan <- rnaseq_plan(metadata_id = config::get("metadata")$synID,
+                    metadata_version = config::get("metadata")$version,
+                    counts_id = config::get("counts")$synID,
+                    counts_version = config::get("counts")$version,
+                    gene_id_input = config::get("counts")$`gene id`,
+                    factor_input = config::get("factors"),
+                    continuous_input = config::get("continuous"),
+                    gene_id = config::get("biomart")$`gene id`,
+                    biomart_id = config::get("biomart")$synID,
+                    biomart_version = config::get("biomart")$version,
+                    filters = config::get("biomart")$filters,
+                    host = config::get("biomart")$host,
+                    organism = config::get("biomart")$organism
+)
+
+drake::make(plan)
 
 # Visualize the results
-drake::vis_drake_graph(execute_plan(config = config),
+drake::vis_drake_graph(plan,
                        targets_only = TRUE)
