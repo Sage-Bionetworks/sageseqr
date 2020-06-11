df02afd8d358f6222938a14171ebca14ee4beb1d
kelshmo <kelsey.montgomery@sagebase.org>
2020-06-11 16:14:51 -0700

    add test with dot notation

--- tests/testthat/test-convert_geneids.R
+++ tests/testthat/test-convert_geneids.R
@@ -6,3 +6,14 @@ test_that("No warning is returned", {
     tibble::column_to_rownames(., var = "genes")
   testthat::expect_silent(convert_geneids(df))
 })
+
+test_that("Transcript Ids removed", {
+  expected_output <- c("ENSG400", "ENSG401", "ENSG402")
+  df <- tibble::tribble(~genes, ~sample1,
+                        "ENSG400.5", 4,
+                        "ENSG401.7", 6,
+                        "ENSG402.8", 10) %>%
+    tibble::column_to_rownames(., var = "genes")
+  output <- convert_geneids(df)
+  testthat::expect_equal(output, expected_output)
+})
