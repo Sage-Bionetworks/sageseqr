abe9d739b08d1f04c7be33746600e263c52864d0
kelshmo <kelsey.montgomery@sagebase.org>
2020-07-20 14:36:54 -0700

    test de with varianceParition dataset

---
+++ tests/testthat/test-de.R
@@ -0,0 +1,26 @@
+library(variancePartition)
+data(varPartDEdata)
+
+countMatrix <- countMatrix[1:100,]
+
+# Add continuous variable
+metadata$AlignmentRate <- 0.9
+
+metadata <- clean_covariates(metadata,
+                             factors = c("Individual", "Disease", "Experiment", "Sex"),
+                             continuous = c("AlignmentRate"))
+
+biomart_results <- tibble::column_to_rownames(
+  tibble::tibble(ensembl_gene_id = rownames(countMatrix), length = 400),
+  var = "ensembl_gene_id"
+  )
+
+test_that("output is a list of 4", {
+  de <- differential_expression(countMatrix,
+                                countMatrix,
+                                md = metadata,
+                                model_variables = c("Individual"),
+                                primary_variable = c("Sex", "Disease"),
+                                biomart_results = biomart_results)
+  expect_equal(length(de), 4)
+})
