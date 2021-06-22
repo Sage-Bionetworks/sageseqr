1a9734df60a09f3f194bad3af0d89ac0a75ccbe1
jgockley62 <jgockley62@gmail.com>
2021-06-22 13:08:45 -0700

    replace vis-func drake::cancel_if

--- R/visualization-functions.R
+++ R/visualization-functions.R
@@ -770,12 +770,13 @@ plot_sexcheck <- function(clean_metadata, count_df, biomart_results, sex_var) {
 #' Conditionally wrap plot_sexcheck for drake
 #'
 #' Work around to expose plot_sexcheck to testing and export but also leverage
-#' drakes function for skipping targets conditionally (see \code{"drake::cancel_if()"}).
+#' targets function for skipping targets conditionally (see \code{"targets::tar_cancel()"}).
 #' @inheritParams plot_sexcheck
 #' @inheritParams get_biomart
 #' @export
 conditional_plot_sexcheck <- function(clean_metadata, count_df, biomart_results, sex_var) {
-  drake::cancel_if(is.null(sex_var))
+  #drake::cancel_if(is.null(sex_var))
+  targets::tar_cancel(is.null(sex_var))
   plot_sexcheck(clean_metadata, count_df, biomart_results, sex_var)
 }
 #' Explore samples that are outliers
