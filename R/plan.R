2df56c2afd761293b661887c1e280441591d768d
kelshmo <kelsey.montgomery@sagebase.org>
2021-07-22 16:45:54 -0500

    revert weird formatting and documentation

--- R/plan.R
+++ R/plan.R
@@ -57,11 +57,11 @@ rnaseq_plan <- function(metadata_id, metadata_version, counts_id,
                                 continuous = !!continuous_input,
                                 sample_identifier = !!sample_id_input),
     biomart_results = get_biomart(count_df = counts,
-                                  synid = !!biomart_id,
-                                  version = !!biomart_version,
-                                  filters = !!filters,
-                                  host = !!host,
-                                  organism = !!organism),
+                                synid = !!biomart_id,
+                                version = !!biomart_version,
+                                filters = !!filters,
+                                host = !!host,
+                                organism = !!organism),
     filtered_counts = filter_genes(clean_metadata = clean_md,
                                    count_df = counts,
                                    conditions = !!conditions,
@@ -88,19 +88,19 @@ rnaseq_plan <- function(metadata_id, metadata_version, counts_id,
                                                                 clean_md),
     outliers = identify_outliers(filtered_counts, clean_md, !!color, !!shape,
                                  !!size),
-    model = stepwise_regression(
-      clean_md,
-      primary_variable = !!primary_variable,
-      cqn_counts = cqn_counts,
-      skip = !!skip_model
-    ),
+     model = stepwise_regression(
+       clean_md,
+       primary_variable = !!primary_variable,
+       cqn_counts = cqn_counts,
+       skip = !!skip_model
+        ),
     report = rmarkdown::render(
       drake::knitr_in("sageseqr-report.Rmd"),
       output_file = drake::file_out(
         !!glue::glue("{getwd()}/{report_name}.html")
-      ),
+        ),
       output_dir = "."
-    ),
+      ),
     document_inputs = provenance_helper(
       !!metadata_id, !!counts_id,
       !!metadata_version, !!counts_version,
@@ -114,13 +114,13 @@ rnaseq_plan <- function(metadata_id, metadata_version, counts_id,
       biomart_results = biomart_results,
       rownames = !!rownames,
       syn_names = list("Covariates", "Filtered counts (greater than 1cpm)",
-                       "BioMart query results", "Normalized counts (CQN)"),
+                    "BioMart query results", "Normalized counts (CQN)"),
       data_names = list("clean_md", "filtered_counts", "biomart_results",
                         "cqn_counts"),
       inputs = document_inputs,
       activity_provenance = "Analyze RNA-seq data with sageseqr pkg",
       config_file = !!config_file,
       report_name = !!report_name
+      )
     )
-  )
-}
+  }
